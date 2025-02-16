42cdc76a49a07ca4c960ba019690a081e24f92a8
YAN HU <yanhu.forever@gmail.com>
2025-02-16 14:36:32 +1100

    index.css

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,16 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import "./index.css"
 
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql', // 替换为你的 GraphQL 服务器地址
-  cache: new InMemoryCache(),
-});
-
-ReactDOM.render(
-  <ApolloProvider client={client}>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </ApolloProvider>,
-  document.getElementById('root')
-);
+  </React.StrictMode>,
+)
